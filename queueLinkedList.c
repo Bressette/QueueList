2507a9df60a811656fba80c320e6e9fd629d791b
Johnathan Bressette <bressetteprogramming@gmail.com>
2019-10-09 08:10:43 -0400

    Initial commit

---
+++ queueLinkedList.c
@@ -0,0 +1,19 @@
+#include "stdlib.h"
+#include "stdio.h"
+
+struct queue
+{
+    int element;
+    struct queue *front;
+    struct queue *rear;
+};
+
+
+int main()
+{
+    struct node *head = NULL;
+
+
+
+    return 0;
+}
